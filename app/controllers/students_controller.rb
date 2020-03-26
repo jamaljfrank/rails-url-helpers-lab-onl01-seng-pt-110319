3e7a54e7197faec2610da82bdf4a9abe029049fa
Jamal Frank <jamaljfrank@gmail.com>
2020-03-26 00:47:13 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
   
   def activate
     @student = Student.find(params[:id])
-    @student.active
+    @student.toggle(:active)
   end
 
   private
